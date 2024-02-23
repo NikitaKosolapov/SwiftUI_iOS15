37c0c832eadc0efca0445845d25b31c6f1338c16
Nikita Kosolapov <kosolapovnikitav@gmail.com>
2024-02-23 09:32:34 +0300

    Initial Commit

---
+++ SwiftUI_iOS15/SwiftUI_iOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_iOS15App.swift
+//  SwiftUI_iOS15
+//
+//  Created by Nikita Kosolapov on 23.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_iOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
